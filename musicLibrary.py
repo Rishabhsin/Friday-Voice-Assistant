0cdc4da1ef9c01ac54f7cf8cbc335ada3bc4da55
RISHABH SINGH <82760483+RISHABHSINGH3006@users.noreply.github.com>
2024-08-02 11:28:42 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=jHpQERs3ktc&list=PLEX0_dFX2_BakyTEXosZrSxrMRC21lRVH&index=3",
+    "march": "https://www.youtube.com/watch?v=CRHsb0usNV4&list=PLEX0_dFX2_BakyTEXosZrSxrMRC21lRVH&index=2",
+    "skyfall": "https://www.youtube.com/watch?v=d7eZsOADCos&list=PLEX0_dFX2_BbkQ4tR5x8CKHsn8xfyVoK6&index=2",
+    "wolf": "https://www.youtube.com/watch?v=2lLwaDK7mEs"
+}
